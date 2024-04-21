c2409e130232a5edbbc32cc296044c4a68348acd
yvictor <yvictor3141@gmail.com>
2024-04-21 14:38:17 +0000

    refactor: event_reader as native rust

--- src/cfapi/src/event_reader.rs
+++ src/cfapi/src/event_reader.rs
@@ -1,5 +1,5 @@
-use super::value::CFValue;
 use super::binding::{GetEventReader, MessageEvent, MessageEvent_Types, MessageReader, ValueTypes};
+use super::value::CFValue;
 use std::collections::BTreeMap;
 
 pub struct EventReaderSerConfig {
@@ -38,7 +38,11 @@ impl<'a> EventReader<'a> {
     pub fn new(event: &'a MessageEvent, ser_config: &'a EventReaderSerConfig) -> Self {
         let reader = GetEventReader(event) as *mut MessageReader;
         let reader = unsafe { std::pin::Pin::new_unchecked(&mut *reader) };
-        EventReader { event, reader, ser_config}
+        EventReader {
+            event,
+            reader,
+            ser_config,
+        }
     }
     pub fn with_ser_config(mut self, ser_config: &'a EventReaderSerConfig) -> Self {
         self.ser_config = ser_config;
@@ -46,8 +50,123 @@ impl<'a> EventReader<'a> {
     }
 }
 
-impl EventReader<'_> {
-    pub fn to_map(&mut self) -> BTreeMap<String, CFValue> {
+pub struct EventReaderWithTokenNumberIter<'a> {
+    reader: &'a mut EventReader<'a>,
+}
+
+impl<'a> Iterator for EventReaderWithTokenNumberIter<'a> {
+    type Item = (i32, CFValue);
+
+    fn next(&mut self) -> Option<Self::Item> {
+        self.reader.next_with_token_number()
+    }
+}
+
+pub struct EventReaderWithTokenNameIter<'a> {
+    reader: &'a mut EventReader<'a>,
+}
+
+impl<'a> Iterator for EventReaderWithTokenNameIter<'a> {
+    type Item = (String, CFValue);
+
+    fn next(&mut self) -> Option<Self::Item> {
+        self.reader.next_with_token_name()
+    }
+}
+
+pub struct EventReaderWithTokenNumNameIter<'a> {
+    reader: &'a mut EventReader<'a>,
+}
+
+impl<'a> Iterator for EventReaderWithTokenNumNameIter<'a> {
+    type Item = (i32, String, CFValue);
+
+    fn next(&mut self) -> Option<Self::Item> {
+        self.reader.next_with_token_num_name()
+    }
+}
+
+impl<'a> Iterator for EventReader<'a> {
+    type Item = CFValue;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        if self.reader.as_mut().next() != autocxx::c_int(-1) {
+            Some(self.get_value())
+        } else {
+            None
+        }
+    }
+}
+
+impl<'a> EventReader<'a> {
+    pub fn get_token_number(&mut self) -> i32 {
+        i32::from(self.reader.as_mut().getTokenNumber())
+    }
+
+    pub fn get_token_name(&mut self) -> String {
+        self.reader.as_mut().getTokenName().to_string()
+    }
+
+    pub fn find(&'a mut self, id: i32) -> Option<CFValue> {
+        if self.reader.as_mut().find(autocxx::c_int(id)) {
+            Some(self.get_value())
+        } else {
+            None
+        }
+    }
+
+    pub fn get_value(&mut self) -> CFValue {
+        match self.reader.as_mut().getValueType() {
+            ValueTypes::INT64 => CFValue::Int(self.reader.as_mut().getValueAsInteger()),
+            ValueTypes::DOUBLE => CFValue::Double(self.reader.as_mut().getValueAsDouble()),
+            ValueTypes::STRING => {
+                CFValue::String(self.reader.as_mut().getValueAsString().to_string())
+            }
+            ValueTypes::DATETIME => CFValue::Datetime(self.reader.as_mut().getValueAsDouble()),
+            ValueTypes::UNKNOWN => CFValue::Unknown,
+        }
+    }
+    pub fn next_with_token_number(&mut self) -> Option<(i32, CFValue)> {
+        if self.reader.as_mut().next() != autocxx::c_int(-1) {
+            Some((self.get_token_number(), self.get_value()))
+        } else {
+            None
+        }
+    }
+
+    pub fn next_with_token_name(&mut self) -> Option<(String, CFValue)> {
+        if self.reader.as_mut().next() != autocxx::c_int(-1) {
+            Some((self.get_token_name(), self.get_value()))
+        } else {
+            None
+        }
+    }
+
+    pub fn next_with_token_num_name(&mut self) -> Option<(i32, String, CFValue)> {
+        if self.reader.as_mut().next() != autocxx::c_int(-1) {
+            Some((
+                self.get_token_number(),
+                self.get_token_name(),
+                self.get_value(),
+            ))
+        } else {
+            None
+        }
+    }
+
+    pub fn iter_with_token_number(&'a mut self) -> EventReaderWithTokenNumberIter {
+        EventReaderWithTokenNumberIter { reader: self }
+    }
+
+    pub fn iter_with_token_name(&'a mut self) -> EventReaderWithTokenNameIter {
+        EventReaderWithTokenNameIter { reader: self }
+    }
+
+    pub fn iter_with_token_num_name(&'a mut self) -> EventReaderWithTokenNumNameIter {
+        EventReaderWithTokenNumNameIter { reader: self }
+    }
+
+    pub fn to_map(&'a mut self) -> BTreeMap<String, CFValue> {
         let mut map = BTreeMap::new();
         if self.ser_config.with_event_type {
             let event_type = self.event.getType() as MessageEvent_Types;
@@ -71,31 +190,19 @@ impl EventReader<'_> {
         }
         let symbol = self.event.getSymbol();
         map.insert("(2)Symbol".to_owned(), CFValue::String(symbol.to_string()));
-        while self.reader.as_mut().next() != autocxx::c_int(-1) {
-            let key = format!(
-                "({}){}",
-                i32::from(self.reader.as_mut().getTokenNumber()),
-                self.reader.as_mut().getTokenName()
-            );
-            let value = match self.reader.as_mut().getValueType() {
-                ValueTypes::INT64 => CFValue::Int(self.reader.as_mut().getValueAsInteger()),
-                ValueTypes::DOUBLE => CFValue::Double(self.reader.as_mut().getValueAsDouble()),
-                ValueTypes::STRING => {
-                    CFValue::String(self.reader.as_mut().getValueAsString().to_string())
-                }
-                ValueTypes::DATETIME => CFValue::Datetime(self.reader.as_mut().getValueAsDouble()),
-                ValueTypes::UNKNOWN => CFValue::Unknown,
-            };
-            map.insert(key, value);
+
+        for (token_number, token_name, value) in self.iter_with_token_num_name() {
+            map.insert(format!("({}){}", token_number, token_name), value);
         }
+
         map
     }
 
-    pub fn to_json(&mut self) -> Result<String, serde_json::Error> {
+    pub fn to_json(&'a mut self) -> Result<String, serde_json::Error> {
         serde_json::to_string(&mut self.to_map())
     }
 
-    pub fn to_msgpack(&mut self) -> Result<Vec<u8>, rmp_serde::encode::Error> {
+    pub fn to_msgpack(&'a mut self) -> Result<Vec<u8>, rmp_serde::encode::Error> {
         rmp_serde::to_vec_named(&self.to_map())
     }
 }
